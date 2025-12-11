48a24f7f8f2a0ed6128a317164585e3ebb584647
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2025-12-11 23:25:13 +0700

    Update SceneSwitcherWindow.cs

--- SceneSwitcherWindow.cs
+++ SceneSwitcherWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -50,3 +51,4 @@ public class SceneSwitcherWindow : EditorWindow
         }
     }
 }
+#endif
